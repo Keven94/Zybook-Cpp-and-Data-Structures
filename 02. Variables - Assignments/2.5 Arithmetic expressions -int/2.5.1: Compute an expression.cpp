7a2447fb8505948fa6d96a12bba8a34cf52a5267
Keven94 <40154212+Keven94@users.noreply.github.com>
2019-01-05 00:48:56 -0600

    Create 2.5.1: Compute an expression.cpp

---
+++ 02. Variables - Assignments/2.5 Arithmetic expressions -int/2.5.1: Compute an expression.cpp
@@ -0,0 +1,25 @@
+/*
+Write a statement that assigns finalResult with the sum of num1 and num2, divided by 3. 
+Ex: If num1 is 4 and num2 is 5, finalResult is 3. 
+*/
+
+
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+   int num1;
+   int num2;
+   int finalResult;
+
+   num1 = 4;
+   num2 = 5;
+
+   /* Your solution goes here  */
+   finalResult = (num1 + num2) / 3;
+
+   cout << "Final result: " << finalResult << endl;
+
+   return 0;
+}
