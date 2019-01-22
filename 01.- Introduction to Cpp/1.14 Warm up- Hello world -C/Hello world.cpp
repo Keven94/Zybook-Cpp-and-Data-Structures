ad518be809ed899b9969170bed6aa6973ef32f4d
Keven94 <40154212+Keven94@users.noreply.github.com>
2019-01-21 23:49:51 -0600

    Create Hello world.cpp

---
+++ 01.- Introduction to Cpp/1.14 Warm up- Hello world -C/Hello world.cpp
@@ -0,0 +1,14 @@
+
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+
+   /* Type your code here. */
+   cout << "Hello world!"    << endl;
+   cout << "How are you?"    << endl;
+   cout << "   (I'm fine)."  << endl;
+
+   return 0;
+}
