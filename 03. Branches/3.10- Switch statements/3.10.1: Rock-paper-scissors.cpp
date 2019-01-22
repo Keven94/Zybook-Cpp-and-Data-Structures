9565682e2be02a22c4131fea4a54de15c249c5a6
Keven94 <40154212+Keven94@users.noreply.github.com>
2019-01-21 23:33:05 -0600

    Create 3.10.1: Rock-paper-scissors.cpp

---
+++ 03. Branches/3.10- Switch statements/3.10.1: Rock-paper-scissors.cpp
@@ -0,0 +1,35 @@
+/*
+Write a switch statement that checks nextChoice. 
+If 0, print "Rock". If 1, print "Paper". If 2, print "Scissors". 
+For any other value, print "Unknown". 
+End with newline. Do not get input from the user; nextChoice is assigned in main(). 
+*/
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+   int nextChoice;
+
+   nextChoice = 2;
+
+   /* Your solution goes here  */
+   switch(nextChoice)
+   {
+      case 0:
+         cout << "Rock" << endl;
+         break;
+      case 1:
+         cout << "Paper" << endl;
+         break;
+      case 2:
+         cout << "Scissors" << endl;
+         break;
+      default:
+         cout << "Unknown" << endl;
+         break;
+   }
+      
+   return 0;
+}
