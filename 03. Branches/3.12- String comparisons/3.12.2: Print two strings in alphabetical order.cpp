334d239864dfa405cab7719536a4d6f00f3ca197
Keven94 <40154212+Keven94@users.noreply.github.com>
2019-01-23 16:36:20 -0600

    Create  3.12.2: Print two strings in alphabetical order.cpp

---
+++ 03. Branches/3.12- String comparisons/3.12.2: Print two strings in alphabetical order.cpp
@@ -0,0 +1,29 @@
+/*
+Print the two strings in alphabetical order. Assume the strings are lowercase. End with newline. Sample output:
+
+capes rabbits
+*/
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+   string firstString;
+   string secondString;
+
+   firstString = "rabbits";
+   secondString = "capes";
+
+   /* Your solution goes here  */
+   if (firstString < secondString)
+   {
+      cout << firstString << " " << secondString << endl;
+   }
+   else
+   {
+      cout << secondString << " " << firstString << endl;
+   }
+
+   return 0;
+}
