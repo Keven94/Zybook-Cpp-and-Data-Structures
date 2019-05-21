d197b943b0861dca17317cda1115a519cf644aa4
Keven94 <40154212+Keven94@users.noreply.github.com>
2019-05-21 17:15:21 -0500

    Create 6.3.2: Function stubs: Statistics.cpp

---
+++ 06. User-Defined Functions/6.03: Reasons for functions/6.3.2: Function stubs: Statistics.cpp
@@ -0,0 +1,39 @@
+/*
+Define stubs for the functions called by the below main(). 
+Each stub should print "FIXME: Finish FunctionName()" followed by a newline, and should return -1. Example output:
+
+FIXME: Finish GetUserNum()
+FIXME: Finish GetUserNum()
+FIXME: Finish ComputeAvg()
+Avg: -1
+*/
+
+#include <iostream>
+using namespace std;
+
+/* Your solution goes here  */
+int GetUserNum()
+{
+   cout << "FIXME: Finish GetUserNum()" << endl;
+}
+
+int ComputeAvg(int num1, int num2)
+{
+   cout << "FIXME: Finish ComputeAvg()" << endl;
+   return -1;
+}
+
+int main() {
+   int userNum1;
+   int userNum2;
+   int avgResult;
+
+   userNum1 = GetUserNum();
+   userNum2 = GetUserNum();
+
+   avgResult = ComputeAvg(userNum1, userNum2);
+
+   cout << "Avg: " << avgResult << endl;
+
+   return 0;
+}
